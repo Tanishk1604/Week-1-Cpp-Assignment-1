1c7e5d3b091cca3d9d5572e67f3419ec6a7dc3b1
Tanishk1604 <145919505+Tanishk1604@users.noreply.github.com>
2024-02-21 18:03:18 +0530

    Add files via upload

---
+++ bool.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    bool flag = true; // when false , value displayed is 0
+    bool hello = false;
+    cout << flag << endl;
+    cout << hello;
+}
