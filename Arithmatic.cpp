5b533b55551cdf85f3c9b69cfa711483ce64b7ee
Tanishk1604 <145919505+Tanishk1604@users.noreply.github.com>
2024-02-21 17:56:59 +0530

    test file

---
+++ Arithmatic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    x = 7;
+    cout << x << endl;
+    x = 10;
+    cout << x << endl;
+    x = x + 10;
+    cout << x << endl;
+    x = x / 2;
+    cout << x << endl;
+    x = x - 2;
+    cout << x << endl;
+    x = x * 4;
+    cout << x << endl;
+    int y;
+    y = x / 8;
+    cout << y;
+}
