71bf9a5737473e8e7de87ee18ba6fa4f58021db7
Tanishk1604 <145919505+Tanishk1604@users.noreply.github.com>
2024-02-21 18:03:39 +0530

    Delete bool.cpp

--- bool.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main()
-{
-    using namespace std;
-    bool flag = true; // when false , value displayed is 0
-    bool hello = false;
-    cout << flag << endl;
-    cout << hello;
-}
