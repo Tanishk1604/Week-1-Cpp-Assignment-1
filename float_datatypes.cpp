51d2d723eb6b96b9bd466d9ec902ba700c833450
Tanishk1604 <145919505+Tanishk1604@users.noreply.github.com>
2024-02-21 17:59:36 +0530

    test 3

---
+++ float_datatypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x = 3.1414;
+    cout << x << endl;
+    x = x + 1;
+    cout << x << endl;
+    int a = 11;
+    int b = 15;
+    cout << a % b;
+}
