8ac1954c47bb93d54397b842506264706a62be93
Tanishk1604 <145919505+Tanishk1604@users.noreply.github.com>
2024-02-21 17:58:01 +0530

    test 2

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World";
+    cout << endl; // \n works same as end line
+    cout << "Hello Tanishk\n";
+    cout << "hello again" << endl;
+    cout << "hello vaze" << endl;
+    cout << "bye";
+}
