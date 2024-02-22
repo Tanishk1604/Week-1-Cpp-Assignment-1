fb9f499849ebcce590785e847a3231edd0ef3f66
Tanishk1604 <145919505+Tanishk1604@users.noreply.github.com>
2024-02-22 18:01:58 +0530

    Output & Variables Assignment

---
+++ Assignment.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Q 1 -
+    cout << "Physics" << endl;
+    cout << "Wallah" << endl;
+
+    // Q 2 -
+    int x = 5;
+    int y = 5;
+    int z;
+    z = x + y;
+    cout << z << endl;
+
+    // Q 3-
+    // int - 4 bytes
+    // bool - 1 bytes
+    // float - 4 bytes
+
+    // Q 4 -
+    int a = 4;
+    int b = 5;
+    a++, b--;
+    cout << ++a << " " << b-- << endl;
+
+    // Output -
+    // 6 4
+
+    // Q 5 -
+    int radius = 10;
+    int c;
+    c = radius * 2 * 3.14;
+    cout << c << endl;
+
+    // Q 6 -
+    // 01Pwskills ---------- NO
+    // _FLOAT -----------YES
+    // int ----------NO
+    // FLOAT --------YES
+    // You will succeed------NO
+}
